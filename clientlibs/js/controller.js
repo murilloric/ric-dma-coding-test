a20999fb4617ac28164007417ed2beba1480fef3
murilloric <murilloric@gmail.com>
2016-07-16 15:31:40 -0700

    js sort by date and name

--- clientlibs/js/controller.js
+++ clientlibs/js/controller.js
@@ -1,5 +1,9 @@
 MASTERAPP.controller('homeCtrl', ['$scope', 'ricsAPI', function ($scope, ricsAPI) {
-	
+	$scope.sort_property = 'title'
+	$scope.sortBy = function(){
+		console.log('yes')
+	}
+
 	ricsAPI.searchForMovies(function(resp){
 		console.log(resp)
 		$scope.movie_data = resp
